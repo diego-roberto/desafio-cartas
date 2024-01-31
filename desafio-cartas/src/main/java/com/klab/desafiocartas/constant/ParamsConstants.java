86972aaedd28ee26bd3b886136d01e454b55df45
Diego Roberto <diego-roberto@hotmail.com>
2024-01-30 23:20:33 -0300

    Classe utilitária de constantes para uso com variáveis de ambiente

---
+++ desafio-cartas/src/main/java/com/klab/desafiocartas/constant/ParamsConstants.java
@@ -0,0 +1,12 @@
+package com.klab.desafiocartas.constant;
+
+public class ParamsConstants {
+
+//    public static final int NUMBER_OF_PLAYERS = 4;
+    public static final String NUMBER_OF_PLAYERS = System.getenv("NUMBER_OF_PLAYERS");
+
+//    public static final int DRAW_SIZE = 5;
+    public static final String DRAW_SIZE = System.getenv("DRAW_SIZE");
+
+
+}
