8de73a0f7c6d6df8ffa93c9e33370fff07b6806a
Diego Roberto <diego-roberto@hotmail.com>
2024-02-01 18:22:42 -0300

    Ajustes nos arquivos de projeto para Testes

--- desafio-cartas/src/main/java/com/klab/desafiocartas/constant/MessageConstants.java
+++ desafio-cartas/src/main/java/com/klab/desafiocartas/constant/MessageConstants.java
@@ -11,8 +11,7 @@ public class MessageConstants {
     public static final String MSG_GAME_CREATED = "Jogo criado com sucesso";
     public static final String MSG_ROUND_SUCCESS = "Cartas sorteadas com sucesso";
     public static final String MSG_NOT_FOUND = "Não encontrado / erro interno";
-    public static final String MSG_DECK_NOT_FOUND = "ID do deck não encontrada";
-    public static final String MSG_ROUND_NOT_FOUND = "ID de Jogo não encontrada";
+    public static final String MSG_ROUND_NOT_FOUND = "ID de rodada não encontrada";
     public static final String MSG_NEW_ROUND_CREATED =  "Nova rodada criada para o Baralho";
     public static final String MSG_NEW_ROUND_ID =  "Novo ID de rodada é";
     public static final String MSG_OLD_ROUND_ID =  "Rodada encerrada! Por favor utilize uma rodada aberta";
