b661efbcf92dd12dc7a1823eec208841630139e5
Diego Roberto <diego-roberto@hotmail.com>
2024-02-11 15:22:31 -0300

    Redução de acoplamento na lógica de rodada

--- desafio-cartas/src/main/java/com/klab/desafiocartas/service/impl/RoundServiceImpl.java
+++ desafio-cartas/src/main/java/com/klab/desafiocartas/service/impl/RoundServiceImpl.java
@@ -61,54 +61,80 @@ public class RoundServiceImpl implements RoundService {
 
     @Override
     public ResponseDTO drawCards(Long roundId) {
-        Round round = findByRoundId(roundId);
+        Round round = getRound(roundId);
 
-        if (round == null) {
-            throw new RoundNotFoundException(MessageConstants.MSG_OLD_ROUND_ID, roundId);
-        }
-
-        Optional<Round> optLastRoundId = roundRepository.findLatestRoundByDeckId(round.getDeckId());
-
-        if (optLastRoundId.isPresent()) {
-            Long lastRoundId = optLastRoundId.get().getRoundId();
-            if (lastRoundId > roundId){
-                throw new OldRoundIdException(MessageConstants.MSG_OLD_ROUND_ID, roundId);
-            }
-        }
-
-        if (round.getRemaining() < (drawCount * playerCount)){
-            round = reshuffleAndCreateNewRound(round);
-        }
-
-        if (round.getPlayers() == null || round.getPlayers().isEmpty()) {
-            round = createPlayersForRound(round);
-        } else {
-            Round fRound = round;
-            List<Player> list = new ArrayList<>(round.getPlayers());
-            list.forEach(player -> {
-                Hand newHand = drawFromDeckToPlayers(fRound);
-                player.setPlayerHand(newHand);
-            });
-            round.setPlayers(list);
-            round = roundRepository.save(round);
-        }
+        round = validateAndDrawCards(round);
 
         boolean hasWinner = round.getWinner().isPresent();
         boolean isTie = !hasWinner;
 
-        PlayerDTO winnerDTO = hasWinner ? playerDTOConverter.toDTO(round.getWinner().get()) : null;
+        PlayerDTO winnerDTO = getWinnerDTO(round, hasWinner);
+        List<PlayerDTO> playersDTO = getPlayersDTO(roundId, hasWinner, winnerDTO, isTie);
 
-        List<PlayerDTO> playersDTO = Collections.EMPTY_LIST;
+        return new ResponseDTO(winnerDTO, playersDTO, hasWinner, isTie, round.getRoundId());
+    }
 
-        if (hasWinner) {
-             playersDTO = playerDTOConverter.toDTOListExcludingWinner(findPlayersByRoundId(roundId), winnerDTO.getPlayerId());
+    private Round getRound(Long roundId) {
+        Round round = findByRoundId(roundId);
+        if (round == null) {
+            throw new RoundNotFoundException(MessageConstants.MSG_OLD_ROUND_ID, roundId);
+        }
+        validateRoundId(round, roundId);
+        return round;
+    }
+
+    private Round validateAndDrawCards(Round round) {
+        round = validateRemainingCards(round);
+        return handlePlayersAndDrawCards(round);
+    }
+
+    private PlayerDTO getWinnerDTO(Round round, boolean hasWinner) {
+        return hasWinner ? playerDTOConverter.toDTO(round.getWinner().get()) : null;
+    }
+
+    private List<PlayerDTO> getPlayersDTO(Long roundId, boolean hasWinner, PlayerDTO winnerDTO, boolean isTie) {
+        List<PlayerDTO> playersDTO = Collections.emptyList();
+        if (hasWinner && winnerDTO != null) {
+            playersDTO = playerDTOConverter.toDTOListExcludingWinner(findPlayersByRoundId(roundId), winnerDTO.getPlayerId());
         }
         if (isTie) {
             playersDTO = findDrawPlayers(roundId);
-            playersDTO.addAll(playersDTO);
         }
+        return playersDTO;
+    }
 
-        return new ResponseDTO(winnerDTO, playersDTO, hasWinner, isTie, round.getRoundId());
+    private void validateRoundId(Round round, Long roundId) {
+        Optional<Round> optLastRoundId = roundRepository.findLatestRoundByDeckId(round.getDeckId());
+        if (optLastRoundId.isPresent()) {
+            Long lastRoundId = optLastRoundId.get().getRoundId();
+            if (lastRoundId > roundId) {
+                throw new OldRoundIdException(MessageConstants.MSG_OLD_ROUND_ID, roundId);
+            }
+        }
+    }
+
+    private Round validateRemainingCards(Round round) {
+        if (round.getRemaining() < (drawCount * playerCount)) {
+            return reshuffleAndCreateNewRound(round);
+        }
+        return round;
+    }
+
+    private Round handlePlayersAndDrawCards(Round round) {
+        if (round.getPlayers() == null || round.getPlayers().isEmpty()) {
+            return createPlayersForRound(round);
+        } else {
+            List<Player> updatedPlayers = round.getPlayers().stream()
+                    .map(player -> {
+                        Hand newHand = drawFromDeckToPlayers(round);
+                        player.setPlayerHand(newHand);
+                        return player;
+                    })
+                    .collect(Collectors.toList());
+
+            round.setPlayers(updatedPlayers);
+            return roundRepository.save(round);
+        }
     }
 
     private Round createPlayersForRound(Round round) {
